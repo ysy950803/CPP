6d1e7e4ed9bfae19487c33557e520b9e4df530e0
Sylvester Yao <admin@ysy950803.cn>
2020-01-14 01:24:05 +0800

    Add ANeuralNetworksDevice methods usage.

--- ANDKDev/app/src/main/cpp/test_nn.cpp
+++ ANDKDev/app/src/main/cpp/test_nn.cpp
@@ -8,9 +8,31 @@ extern "C" {
 #endif
 
 JNIEXPORT void JNICALL test_nn() {
-    uint32_t *numDevices = nullptr;
-    int res = ANeuralNetworks_getDeviceCount(numDevices);
-    LOGD("res: %d", res);
+    uint32_t numDevices = 0;
+    int ret = ANeuralNetworks_getDeviceCount(&numDevices);
+    LOGI("getDeviceCount %d %d", ret, numDevices);
+
+    for (uint32_t i = 0; i < numDevices; i++) {
+        ANeuralNetworksDevice *device = nullptr;
+        ret = ANeuralNetworks_getDevice(i, &device);
+        LOGI("getDevice %d", ret);
+
+        const char *name = nullptr;
+        ret = ANeuralNetworksDevice_getName(device, &name);
+        LOGI("getName %d %s", ret, name);
+
+        int32_t type = 0;
+        ret = ANeuralNetworksDevice_getType(device, &type);
+        LOGI("getType %d %d", ret, type);
+
+        const char *version = nullptr;
+        ret = ANeuralNetworksDevice_getVersion(device, &version);
+        LOGI("getVersion %d %s", ret, version);
+
+        int64_t featureLevel = 0;
+        ret = ANeuralNetworksDevice_getFeatureLevel(device, &featureLevel);
+        LOGI("getFeatureLevel %d %lld", ret, featureLevel);
+    }
 }
 
 #ifdef __cplusplus
